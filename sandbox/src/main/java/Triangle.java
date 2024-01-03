5ff4850d4674ac659bdea8f453c862abab86f5f0
roman <roman.makarenko.spb@gmail.com>
2024-01-03 23:42:31 +0300

    Task 3 ()

--- sandbox/src/main/java/Triangle.java
+++ sandbox/src/main/java/Triangle.java
@@ -1,3 +1,5 @@
+import org.example.MyException;
+
 public class Triangle {
 
     public double side1;
@@ -5,23 +7,30 @@ public class Triangle {
     public double side3;
     public double halfPerimeter;
 
-    public Triangle (double side1, double side2, double side3) {
+    public Triangle (double side1, double side2, double side3) throws MyException {
 
         this.side1 = side1;
         this.side2 = side2;
         this.side3 = side3;
         this.halfPerimeter = perimeter() / 2;
 
+        if (this.side1 <= 0 || this.side2 <= 0 || this.side3 <= 0) {
+            throw new MyException("Triangle can not have negative sides!");}
+
+        if (this.side1 + this.side2 <= this.side3 || this.side2 + this.side3 <= this.side1 || this.side1 + this.side2 <= this.side2) {
+            throw new MyException("Triangle is impossible to have such sides!");}
     }
 
-  public double perimeter () {
+    public double perimeter () {
       return this.side1 + this.side2 + this.side3;
-  }
+    }
 
   public double square () {
       return Math.sqrt(this.halfPerimeter * (this.halfPerimeter - this.side1) * (this.halfPerimeter - this.side2) * (this.halfPerimeter - this.side3));
   }
 }
+
+
 
 
 
--- sandbox/src/test/java/TriangleTest.java
+++ sandbox/src/test/java/TriangleTest.java
@@ -1,3 +1,4 @@
+import org.example.MyException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -5,14 +6,34 @@ public class TriangleTest {
 
     @Test
 
-    void perimeterTest() {
+    void perimeterTest() throws MyException {
         Triangle firstTriangle = new Triangle(3, 2, 1);
         Assertions.assertEquals(6, firstTriangle.perimeter());
     }
 
     @Test
-    void squareTest () {
+    void squareTest () throws MyException {
         Triangle secondTriangle = new Triangle(3, 4, 5);
         Assertions.assertEquals(secondTriangle.square(), 6 );
+    }
+
+    @Test
+    void negativeSideTest () throws MyException {
+        try {
+            Triangle negativeTriangle = new Triangle(-1, 0, 2);
+        }
+        catch (MyException exception) {
+            System.out.println("Negative side is detected");
+        }
+    }
+
+    @Test
+    void sidesSumTest () throws MyException {
+        try {
+            Triangle sideSumTriangle = new Triangle(1, 2, 4);
+        }
+        catch (MyException exception) {
+            System.out.println("Impossible triangle, sum of 2 sides is less than the third side");
+        }
     }
 }
