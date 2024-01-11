473414b29ad1bc97a3ed36f270921f50c6962719
roman <roman.makarenko.spb@gmail.com>
2024-01-12 02:01:31 +0300

    Task 5 web autotest Changes due to Alexei`s comments

--- addressbook_web_tests/src/test/java/AddDeleteGroupTest.java
+++ addressbook_web_tests/src/test/java/AddDeleteGroupTest.java
@@ -2,16 +2,16 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.util.*;
 
-public class AdddeletegroupTest {
+public class AddDeleteGroupTest {
   private WebDriver driver;
   private Map<String, Object> vars;
   JavascriptExecutor js;
@@ -36,7 +36,7 @@ public class AdddeletegroupTest {
   }
 
   @Test
-  public void adddeletegroup() throws InterruptedException {
+  public void addNewGroup() throws InterruptedException {
     driver.get("http://localhost/addressbook/addressbook/group.php");
     driver.manage().window().setSize(new Dimension(1296, 688));
     driver.findElement(By.name("user")).sendKeys("admin");
@@ -53,12 +53,45 @@ public class AdddeletegroupTest {
     driver.findElement(By.name("group_footer")).sendKeys("bar");
     driver.findElement(By.name("submit")).click();
     System.out.println("Group created successful");
+    driver.findElement(By.linkText("Logout")).click();
 
-    driver.findElement(By.linkText("group page")).click();
+  }
+
+  @Test
+  public void deleteGroup() throws InterruptedException {
+    driver.get("http://localhost/addressbook/addressbook/group.php");
+    driver.manage().window().setSize(new Dimension(1296, 688));
+    driver.findElement(By.name("user")).sendKeys("admin");
+    driver.findElement(By.name("pass")).sendKeys("secret");
+    driver.findElement(By.xpath("/html/body/div/div[4]/form/input[3]")).click();
+    System.out.println("Login successful");
+
+    //check if there is no group - create it
+
+    if (!isElementPresent(By.name("Selected[]"))) {
+      driver.findElement(By.name("new")).click();
+      driver.findElement(By.name("group_name")).sendKeys("test group");
+      driver.findElement(By.name("group_header")).sendKeys("foo");
+      driver.findElement(By.name("group_footer")).sendKeys("bar");
+      driver.findElement(By.name("submit")).click();
+      System.out.println("Group created successful");
+    }
+
+    driver.findElement(By.linkText("group page")).click();  // go to groups page
     driver.findElement(By.cssSelector(".group:nth-child(6) > input")).click();
     driver.findElement(By.cssSelector("input:nth-child(9)")).click();
     driver.findElement(By.cssSelector("html")).click();
     driver.findElement(By.linkText("Logout")).click();
 
+  }
+
+  private boolean isElementPresent(By locator) {
+
+    try {
+      driver.findElement(locator);
+      return true;
+    } catch (NoSuchElementException exception) {
+      return false;
+    }
   }
 }
